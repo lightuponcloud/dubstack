0e1a58c2dbe80b5c1ecef9eb86aa71a14b0ada87
IT-Knight <66753668+IT-Knight@users.noreply.github.com>
2021-03-02 16:07:20 +0200

    leave only tests

--- c_src/env.mk
+++
@@ -1,3 +0,0 @@
-ERTS_INCLUDE_DIR ?= /usr/lib/erlang/erts-9.1.3/include/
-ERL_INTERFACE_INCLUDE_DIR ?= /usr/lib/erlang/lib/erl_interface-3.10/include
-ERL_INTERFACE_LIB_DIR ?= /usr/lib/erlang/lib/erl_interface-3.10/lib
