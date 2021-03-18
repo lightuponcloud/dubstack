b226a40750b0d380ef5ee3f15030a3dc05502d18
IT-Knight <66753668+IT-Knight@users.noreply.github.com>
2021-03-18 20:04:15 +0200

    tests opertion RENAME

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -15,7 +15,6 @@ class RenameTest(unittest.TestCase):
     #
     #
     # To make sure error returned in the following cases.
-    #
     # * rename directory to the name of existing file
     # * rename file to the name of existing directory
     #
@@ -145,7 +144,7 @@ class RenameTest(unittest.TestCase):
                 pseudo_dir = obj
                 break
         else:
-            assert False
+            raise Exception('Something went wrong')
 
         # print(hex_decoder(dir['prefix'].split('/')[1]).split("-")[0])
         dst_name = generate_random_name()
@@ -159,6 +158,260 @@ class RenameTest(unittest.TestCase):
 
         # Clean: delete all created
         self.client.delete(TEST_BUCKET_1, [prefixes[0]])
+
+    def test_negative_case4(self):
+        """
+        # To make sure error returned in the following cases:
+        # * rename directory to the name of existing file
+        # * rename file to the name of existing directory
+        """
+        # 1. Upload a file and create a directory
+        dir_name = generate_random_name()
+        dir_name_prefix = encode_to_hex(dir_name)
+        self.client.create_pseudo_directory(TEST_BUCKET_1, dir_name)
+        fn = "20180111_165127.jpg"
+        res = self.client.upload(TEST_BUCKET_1, fn)
+        object_key = res['object_key']
+        orig_name = res["orig_name"]
+
+        # 2.1 Try rename directory to the name of existing file
+        res = self.client.rename(TEST_BUCKET_1, dir_name_prefix, orig_name)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 29})  # "29": "Object exists already.",
+
+        # 2.2 Try rename file to the name of existing directory
+        res = self.client.rename(TEST_BUCKET_1, object_key, dir_name)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 10})  # "10": "Directory exists already.",
+
+        # Clean: delete all created
+        self.client.delete(TEST_BUCKET_1, [dir_name_prefix, object_key])
+
+    def test_negative_case5(self):
+        """
+        # 1. rename file
+        # 2. create directory with the same name
+        # 3. make sure error appears
+        """
+        # 1. Upload file
+        fn = "20180111_165127.jpg"
+        res = self.client.upload(TEST_BUCKET_1, fn)
+        object_key = res['object_key']
+
+        # 2. Rename it
+        random_name = generate_random_name()
+        res = self.client.rename(TEST_BUCKET_1, object_key, random_name)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json()['orig_name'], random_name)
+
+        # 3. Create directory with the same name
+        dir_name = random_name
+        res = self.client.create_pseudo_directory(TEST_BUCKET_1, dir_name)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 29})  # "29": "Object exists already.",
+
+        # Clean: delete all created
+        res = self.client.get_list(TEST_BUCKET_1)
+        for el in res.json()['list']:
+            if el['orig_name'] == random_name:
+                object_key = el['object_key']
+                break
+        else:
+            raise Exception('File gone somewhere')
+
+        self.client.delete(TEST_BUCKET_1, [object_key])
+
+    def test_negative_case6(self):
+        """
+        # 1. rename file to the same name but with different case ( uppercase / lowercase )
+        # 2. to make sure no server call made
+        """
+        # 1. Upload 2 files with upper and lowercase names
+        fn1 = "requirements.txt"
+        fn2 = "README.md"
+        res = self.client.upload(TEST_BUCKET_1, fn1)
+        object_key1 = res['object_key']
+        orig_name1 = res["orig_name"]
+        res = self.client.upload(TEST_BUCKET_1, fn2)
+        object_key2 = res['object_key']
+        orig_name2 = res["orig_name"]
+
+        # 2.1 Try rename file 1 to the same name lowercase -> uppercase
+        res = self.client.rename(TEST_BUCKET_1, object_key1, orig_name1.upper())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 29})  # "29": "Object exists already.",
+
+        # 2.2 Try rename file 2 to the same name uppercase -> lowercase
+        res = self.client.rename(TEST_BUCKET_1, object_key2, orig_name2.lower())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 29})
+
+        # Clean: delete all created
+        self.client.delete(TEST_BUCKET_1, [object_key1, object_key2])
+
+    def test_negative_case7(self):
+        """
+        # 1. to create directory on client in UPPERCASE when LOWERCASE exists on server
+        # 2. to make sure files put in NTFS directory uploaded to remote dir using server name,
+        #    not client name.
+        """
+        # 1.1 Create directory on lowercase
+        dir_name_lower = generate_random_name().lower()
+        dir_prefix = encode_to_hex(dir_name_lower)
+        self.client.create_pseudo_directory(TEST_BUCKET_1, dir_name_lower)
+
+        # 1.2 Create directory on same but uppercase
+        dir_name_upper = dir_name_lower.upper()
+        res = self.client.create_pseudo_directory(TEST_BUCKET_1, dir_name_upper)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 10})
+
+        # Clean: delete all created
+        self.client.delete(TEST_BUCKET_1, [dir_prefix])
+
+        # 2.1 Upload a file in lowercase, and try to upload same file with new version in uppercase
+        fn = "requirements.txt"
+        res = self.client.upload(TEST_BUCKET_1, fn)
+        version = res['version']
+
+        fn2 = 'REQUIREMENTS.Txt'
+        res = self.client.upload(TEST_BUCKET_1, fn2, last_seen_version=version)
+        self.assertEqual(res['orig_name'], fn2)
+        self.assertNotEqual(version, res['version'])
+
+        # Clean: delete all created
+        self.client.delete(TEST_BUCKET_1, [res['object_key']])
+
+    def test_negative_case8(self):
+        """
+        # 1. to lock file
+        # 2. to rename it from other user2
+        # 3. make sure it is not renamed
+        """
+
+        # 1. Upload a file
+        fn = '025587.jpg'
+        res = self.client.upload(TEST_BUCKET_1, fn)
+        object_key = res['object_key']
+
+        # 2. Lock uploaded file
+        res = self.client.patch(TEST_BUCKET_1, 'lock', [object_key])
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json()[0]['is_locked'], True)
+
+        # 3. Try to rename uploaded locked file from user2
+        self.client.login(USERNAME_2, PASSWORD_2)
+        random_name = generate_random_name()
+        res = self.client.rename(TEST_BUCKET_1, object_key, random_name)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {'error': 43})  # "43": "Locked",
+
+        # Clean: delete all created
+        self.client.login(USERNAME_1, PASSWORD_1)
+        res = self.client.patch(TEST_BUCKET_1, 'unlock', [object_key])
+        self.client.delete(TEST_BUCKET_1, [object_key])
+
+    def test_negative_case9(self):
+        """
+        # 1. to upload two files
+        # 2. to lock second file
+        # 3. to rename one of them to the name of the second one
+        # 4. to make sure rename is not allowed
+        """
+
+        # 1. Upload 2 files
+        fn1 = '025587.jpg'
+        res = self.client.upload(TEST_BUCKET_1, fn1)
+        obj_key1 = res['object_key']
+        fn2 = 'requirements.txt'
+        res = self.client.upload(TEST_BUCKET_1, fn2)
+        obj_key2 = res['object_key']
+
+        # 2. Lock second uploaded file
+        self.client.patch(TEST_BUCKET_1, 'lock', [obj_key2])
+
+        # 3. Rename first file to name of second
+        res = self.client.rename(TEST_BUCKET_1, obj_key1, fn2)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.json(), {"error": 29})  # "29": "Object exists already.",
+
+        # Clean: delete all created
+        self.client.patch(TEST_BUCKET_1, 'unlock', [obj_key2])
+        self.client.delete(TEST_BUCKET_1, [obj_key1, obj_key2])
+
+    def test_negative_case10(self):
+        """
+        # 1. upload file
+        # 2. rename it
+        # 3. rename it to its key
+        # 4. file should not disappear
+        """
+        # 1. Upload file
+        fn = '025587.jpg'
+        res = self.client.upload(TEST_BUCKET_1, fn)
+        object_key = res['object_key']
+
+        # 2. Rename uploaded file
+        random_name = generate_random_name()
+        res = self.client.rename(TEST_BUCKET_1, object_key, random_name)
+        orig_name = res.json()['orig_name']
+
+        # 3. Try rename renamed file to its object key
+        res = self.client.get_list(TEST_BUCKET_1)
+        for el in res.json()['list']:
+            if el['orig_name'] == orig_name:
+                object_key2 = el['object_key']
+                print(object_key2)
+                break
+        else:
+            raise Exception('Uploaded renamed file gone somewhere')
+
+        res = self.client.rename(TEST_BUCKET_1, object_key2, object_key2)
+        print(res)
+        print(res.json())
+
+        # 4. Check for file is not disapeared
+        res = self.client.get_list(TEST_BUCKET_1)
+        for el in res.json()['list']:
+            if el['orig_name'] == object_key2:
+                obj = el
+                object_key3 = el['object_key']
+                print(object_key3)
+                break
+        self.assertEqual(obj['orig_name'], object_key2)
+
+        # Clean: delete all created
+        self.client.delete(TEST_BUCKET_1, [object_key3, object_key2])
+
+    def test_negative_case11(self):
+        """
+        # 1. rename object to the key that exists in destination directory
+        # 2. move to the destination directory
+        # 3. previous object, with different orig name, should not be replaced
+        """
+        # 1 Upload a file
+        fn = "USER.jpg"
+        res = self.client.upload(TEST_BUCKET_1, fn)
+        object_key1 = res["object_key"]
+        orig_name1 = res["orig_name"]
+
+        # 2 Upload another one
+        res = self.client.upload(TEST_BUCKET_1, object_key1)
+        object_key2 = res["object_key"]
+        orig_name2 = res["orig_name"]
+
+        # 3 Check for first file is not replaced
+        res = self.client.get_list(TEST_BUCKET_1)
+        files = 0
+        for el in res.json()['list']:
+            if el['orig_name'] == orig_name1:
+                files += 1
+            if el['orig_name'] == orig_name2:
+                files += 1
+        self.assertEqual(files, 2)
+
+        # Clean: delete all created
+        self.client.delete(TEST_BUCKET_1, [object_key1, object_key2])
 
 
 if __name__ == '__main__':
